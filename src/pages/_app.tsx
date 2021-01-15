1f57b1b8fac8511d4e9422072914ff380905d73b
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-15 23:51:08 +0200

    create PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,33 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import 'tailwindcss/tailwind.css';
 import 'src/styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const APP_NAME = 'Next.JS Starter';
+const APP_DESCRIPTION = 'Next.JS Starter created by GedasFX';
 
-export default MyApp;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="description" content={APP_DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
