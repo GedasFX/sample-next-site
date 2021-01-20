dca6182d03e0291c65ae8a03c32b57421fc4293a
Gediminas Milašius <gediminasmilasius@gmail.com>
2021-01-20 21:55:58 +0200

    firebase init

---
+++ src/pages/server.tsx
@@ -0,0 +1,22 @@
+import { GetServerSideProps } from 'next';
+import Layout from 'src/layout';
+
+type Props = {
+  message: string;
+};
+
+export default function ServerPage(props: Props) {
+  return (
+    <Layout>
+      <span>Message from server side: {props.message}</span>
+    </Layout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {
+      message: process.env.HW as string,
+    },
+  };
+};
